9a61ea3a05107362cc12525f45b3c7b85060f592
Michael <michaelmckn4650@gmail.com>
2020-11-03 16:02:29 +0000

    Add files via upload

---
+++ websiteExample.js
@@ -0,0 +1,8 @@
+
+
+//button to scroll to the product section
+var button = document.getElementById("button1").addEventListener("click", function() {
+	document.querySelector('#imgul').scrollIntoView({ 
+  behavior: 'smooth' 
+});})
+
